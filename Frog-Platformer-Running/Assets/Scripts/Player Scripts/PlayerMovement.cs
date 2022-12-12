e506adca590cc03b45959efc05ca72b6f13a6175
ThuanNguyenLAB <nguyencuuthuan2001@gmail.com>
2022-12-12 13:55:53 +0200

    Player jump

--- Frog-Platformer-Running/Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Frog-Platformer-Running/Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -5,8 +5,15 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float movementSpeed = 5f;
+    public float jumpPower = 10f;
+    public float secondJumpPower = 10f;
+    public Transform groundCheckPosition;
+    public float radius = 0.3f;
+    public LayerMask layerGround;
 
     private Rigidbody _myBody;
+    private bool _isGrounded;
+    private bool _playerJumped;
 
     void Awake()
     {
